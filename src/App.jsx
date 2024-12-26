6f3b41621a85f5e38224a0a4de8c6a56408be4bb
jh_lee <juhyean8723@gmail.com>
2024-12-26 15:19:40 +0900

    vite.config proxy 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import axios from "axios";
 import "./App.css";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8080",
+// });
+
+// api.get("/api/users").then(function (response) {
+//   console.log(response.data);
+// });
+
+axios.get("/api/users").then(function (response) {
+  console.log(response.data);
+});
 
 function App() {
   return <></>;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  /* 프록시 서버 설정 */
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080", // API 서버 주소
+        changeOrigin: true, // Origin 헤더 변경
+        secure: false, // https: true, http: false
+      },
+    },
+  },
+});
